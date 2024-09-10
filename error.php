08b8343f7132c360699bed7c4d73651eb45cdf9c
Bernhard Froehler <bernhard.froehler@fh-wels.at>
2024-09-10 11:55:42 +0200

    Import flatpak repo

---
+++ error.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Error!</title>
+	</head>
+	<body>
+		<h1>Error</h1>
+		An error has occurred. Please try again to reach the desired page via the start page (<a href="https://3dct.github.io/flatpak/">flatpak.3dct.at</a>).<br/>
+
+		If the error still persists, please contact us, for example through <a href="https://github.com/3dct/open_iA#contact">the contact information in open_iA's README</a> or on its <a href="https://github.com/3dct/open_iA/issues">github issue tracker</a>!
+	</body>
+</html>
